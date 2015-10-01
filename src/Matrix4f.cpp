b41befb47f32779c13b7674b0a7913fa6dad58ae
Bryce Kopankiewitz <brycekopan@gmail.com>
2015-10-01 02:57:55 -0500

    Matrix4f multiplication  improvement

--- src/Matrix4f.cpp
+++ src/Matrix4f.cpp
@@ -26,10 +26,7 @@ inline Matrix4f Matrix4f::operator*(const Matrix4f &b) const
 	Matrix4f c;
 	for(int i = 0; i < 4; i++){
 		for(int k = 0; k < 4; k++){
-			c[k][i] = 0;
-			for(int j = 0; j < 4; j++){
-				c[k][i] = c[k][i] + a[j][i] * b[k][j];
-			}
+			c[k][i] = a[0][i] * b[k][0] + a[1][i] * b[k][1] + a[2][i] * b[k][2] + a[3][i] * b[k][3]
 		}
 	}
 	return c;
